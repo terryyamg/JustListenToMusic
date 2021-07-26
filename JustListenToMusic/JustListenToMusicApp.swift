4cf7670f8516206cfc787dafe0dd89a22f7eb918
terry yang <terry.yang.dudoo@gmail.com>
2021-07-26 13:54:00 +0800

    Initial Commit

---
+++ JustListenToMusic/JustListenToMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustListenToMusicApp.swift
+//  JustListenToMusic
+//
+//  Created by 楊鎮齊 on 2021/7/26.
+//
+
+import SwiftUI
+
+@main
+struct JustListenToMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
